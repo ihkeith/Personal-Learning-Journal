0c8bf4ee08177c86f2908dbe6d7efc8c18885b48
Ian Keith <ian@ianhkeith.com>
2019-05-28 12:16:21 -0500

    Incremental update

--- models.py
+++ models.py
@@ -1,1 +1,33 @@
-from flask import Flask,
+import datetime
+
+from flask_bcrypt import generate_password_hash
+from flask_login import UserMixin
+from peewee import *
+
+DATABASE = SqliteDatabase("journal.dbs")
+
+class User(UserMixin, Model):
+    username = CharField(unique=True)
+    password = CharField(max=100)
+
+    class Meta:
+        database = DATABASE
+    
+    @classmethod
+    def create_user(cls, email, password):
+        """Create a new user"""
+        try:
+            with DATABASE.transaction():
+                cls.create(
+                    email=email,
+                    password=generate_password_hash(password)
+                )
+        else IntegrityError:
+            raise ValueError("User already exists!")
+
+class Entry(Model):
+    title = CharField(max_length=100)
+    date = DateTimeField(default=datetime.datetime.now)
+    time_spent = IntegerField()
+    learned = TextField()
+    resources = CharField()
